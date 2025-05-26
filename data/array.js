ea5ca7c27b5e8e1641ed97a89c91a3c7c531a59a
qsrdev <matteo.albertini747@gmail.com>
2025-05-26 15:39:09 +0200

    first commit

---
+++ data/array.js
@@ -0,0 +1,39 @@
+const consoles = [
+  {
+    id: 1,
+    titolo: "Game Boy Advance: La Rinascita del Gioco Portatile",
+    contenuto: "Il Game Boy Advance ha rivoluzionato il gioco portatile nei primi anni 2000. Con titoli iconici come 'Metroid Fusion' e 'The Legend of Zelda: The Minish Cap', ha segnato un'epoca.",
+    immagine: "./imgs/gba.jpg",
+    tags: ["Game Boy Advance", "Nintendo", "Retrogaming", "Portatile"],
+  },
+  {
+    id: 2,
+    titolo: "Game Boy SP: Stile e Illuminazione in un'unica console",
+    contenuto: "Il Game Boy Advance SP ha introdotto un design a conchiglia e uno schermo retroilluminato, cambiando per sempre l'esperienza di gioco portatile.",
+    immagine: "./imgs/gbasp.jpg",
+    tags: ["Game Boy SP", "Nintendo", "Retro", "Hardware"],
+  },
+  {
+    id: 3,
+    titolo: "PlayStation 2: L’Impero dei JRPG e dei Platform",
+    contenuto: "La PS2 ha una delle librerie più vaste di sempre, con capolavori come 'Shadow of the Colossus' e 'Final Fantasy X'. Un pilastro del retrogaming moderno.",
+    immagine: "./imgs/ps2.jpg",
+    tags: ["PlayStation 2", "Sony", "JRPG", "Platform", "Retrogaming"],
+  },
+  {
+    id: 4,
+    titolo: "Nintendo DS: Due Schermi, Infinite Possibilità",
+    contenuto: "Con il touch screen e giochi come 'The World Ends With You' e 'New Super Mario Bros', il DS ha ridefinito il concetto di portatile.",
+    immagine: "./imgs/ds.jpg",
+    tags: ["Nintendo DS", "Touch", "Innovazione", "Retro", "Nintendo"],
+  },
+  {
+    id: 5,
+    titolo: "Xbox 360: Il salto nella generazione HD",
+    contenuto: "L'Xbox 360 ha segnato l’inizio dell’era HD con titoli come 'Halo 3' e 'Gears of War', ed è ancora molto amato nella scena del retrogaming moderno.",
+    immagine: "./imgs/xbox360.jpg",
+    tags: ["Xbox 360", "Microsoft", "HD", "FPS", "Retrogaming"],
+  },
+];
+
+export default consoles;
